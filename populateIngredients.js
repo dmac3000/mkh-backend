7b7cd0efffbe3dd75a35fe0c14cf1ea801eeb4a0
David McArthur <dmcarthur86@gmail.com>
2023-08-05 13:29:24 +1000

    updated database connection link in populateingredients.js to live backend

--- populateIngredients.js
+++ populateIngredients.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const Ingredient = require('./models/Ingredient');
 
-mongoose.connect('mongodb://localhost:27017/myKitchenHyrules', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error(err));
 
